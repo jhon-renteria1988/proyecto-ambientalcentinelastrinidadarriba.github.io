43994cc45c7a3c843708312bf2267b8c4d33c2aa
jhon-renteria1988 <renteriatruelove@gmail.com>
2025-04-19 10:30:25 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Formulario enviado correctamente. ¡Gracias por contactarnos!");
+});
